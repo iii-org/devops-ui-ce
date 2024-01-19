65726d66076c08ce28067e26e670fdee9272fe06
GitLab CI <gitlab-ci@dev7.iiidevops.org>
2024-01-19 11:05:24 +0000

    Sync from full by bot

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,4 +5,9 @@ ROOT_DIR=/usr/share/nginx/html
 for file in $ROOT_DIR/static/js/*.js $ROOT_DIR/index.html; do
   sed -i 's|VUE_APP_KC_URL_PLACEHOLDER|'${KEYCLOACK_URL}'|g' $file
   sed -i 's|VUE_APP_GTM_CONTAINER_ID_PLACEHOLDER|'${VUE_APP_GTM_CONTAINER_ID}'|g' $file
+  sed -i 's|VUE_APP_SENTRY_ENABLE|'${ENABLE_SENTRY}'|g' $file
+  sed -i 's|VUE_APP_SENTRY_AUTH_TOKEN|'${TOKEN_AUTH}'|g' $file
+  sed -i 's|VUE_APP_SENTRY_URL|'${URL_SENTRY}'|g' $file
+  sed -i 's|VUE_APP_SENTRY_PROJECT|'${PROJECT_SENTRY}'|g' $file
+  sed -i 's|VUE_APP_SENTRY_DSN|'${DSN_SENTRY}'|g' $file
 done
